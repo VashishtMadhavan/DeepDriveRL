9ed71954392e5ded1e302cf00403cad6ae3f6dac
= <=>
2017-05-03 16:51:02 -0700

    bug fixes in run_dqn

--- run_dqn.py
+++ run_dqn.py
@@ -20,8 +20,8 @@ Setting up learning rate scheduler and exploration criterion
 """
 def setup(env, args):
     args.lr_schedule = PiecewiseSchedule([
-    (0,1e-4 * args.lr_mult),
-    (args.max_iters / 4, 1e-4 * args.lr_mult),
+    (0, 1e-4 * args.lr_mult),
+    (args.max_iters / 10, 1e-4 * args.lr_mult),
     (args.max_iters / 2,  5e-5 * args.lr_mult),
     ], outside_value=5e-5 * args.lr_mult)
 
@@ -40,11 +40,6 @@ def setup(env, args):
     args.snapshot_dir = os.path.join(args.output_dir, "weights")
     if not os.path.exists(args.snapshot_dir):
         os.mkdir(args.snapshot_dir)
-
-    args.summary_dir = os.path.join(args.output_dir, "summary")
-    if not os.path.exists(args.summary_dir):
-        os.mkdir(args.summary_dir)
-
 
     if args.task == "DuskDrive":
         env = Logger(env)
@@ -88,7 +83,26 @@ def reward_from_obs(obs, reward, done):
     #print(reward)
     return reward
 
+def task_input_shape(task):
+    if task == "DuskDrive":
+        return (128, 128, 3)
+    elif task == "Torcs":
+        return (64, 64, 3)
+    elif task == "Torcs_novision":
+        return (1, 1, 70)
     
+
+def task_actions(task):
+    if task == "DuskDrive":
+        actions = env.action_space.actions
+        num_actions = len(actions)
+    elif task == "Torcs":
+        actions = [-1, 0, 1]
+        num_actions = 3
+    elif task == "Torcs_novision":
+        actions = [-1, 0, 1]
+        num_actions = 3
+    return actions, num_actions
 
 def train(env, session, args,
     replay_buffer_size=100000,
@@ -100,25 +114,10 @@ def train(env, session, args,
     target_update_freq=1000,
     grad_norm_clipping=10):
     
-    # resizing all network input to (128,128,3) for ease of processing
-    if args.task == "DuskDrive":
-        img_h, img_w, img_c = (128, 128, 3)
-    elif args.task == "Torcs":
-        img_h, img_w, img_c = (64, 64, 3)
-    elif args.task == "Torcs_novision":
-        img_h, img_w, img_c = (1, 1, 70)
+    img_h, img_w, img_c = task_input_shape(args.task)
     input_shape = (img_h, img_w, frame_history_len * img_c)
     
-    if args.task == "DuskDrive":
-        actions = env.action_space.actions
-        num_actions = len(actions)
-    elif args.task == "Torcs":
-        actions = [-1, 0, 1]
-        num_actions = 3
-    elif args.task == "Torcs_novision":
-        actions = [-1, 0, 1]
-        num_actions = 3
-
+    actions, num_actions = task_actions(args.task)
 
     # Placeholder Formatting
     obs_t_ph = tf.placeholder(tf.uint8, [None] + list(input_shape))
@@ -131,14 +130,13 @@ def train(env, session, args,
     obs_tp1_float = tf.cast(obs_tp1_ph, tf.float32) / 255.0
 
     # Q learning dynamics
-    actions_mat = tf.one_hot(act_t_ph, num_actions, off_value=0.0, on_value=1.0, axis=-1)
+    actions_mat = tf.one_hot(act_t_ph, num_actions)
     q_net = args.q_func(obs_t_float, num_actions, scope='q_func', reuse=False)
     target_q_net= args.q_func(obs_tp1_float, num_actions, scope='tq_func', reuse=False)
 
-    q_val = tf.reduce_sum(q_net * actions_mat, reduction_indices=1)
-    target_q_val = rew_t_ph + gamma * tf.reduce_max(target_q_net, reduction_indices=1) * done_mask_ph
+    q_val = tf.reduce_sum(q_net * actions_mat, axis=1)
+    target_q_val = rew_t_ph + gamma * tf.reduce_max(target_q_net, axis=1) * done_mask_ph
     error = tf.reduce_mean(tf.square(target_q_val - q_val))
-    tf.summary.scalar("Train Error", error)
 
     q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q_func')
     target_q_func_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='tq_func')
@@ -148,8 +146,6 @@ def train(env, session, args,
     opt = args.optimizer.constructor(learning_rate=lr, **args.optimizer.kwargs)
     train_fn = minimize_and_clip(opt, error, var_list=q_func_vars, clip_val=grad_norm_clipping)
 
-    merged = tf.summary.merge_all()
-    train_writer = tf.summary.FileWriter(args.summary_dir)
 
     update_target_fn = []
     for var, var_target in zip(sorted(q_func_vars, key=lambda v: v.name), 
@@ -167,30 +163,29 @@ def train(env, session, args,
     num_param_updates = 0
     mean_episode_reward      = -float('nan')
     best_mean_episode_reward = -float('inf')
-    episode_rewards = []
-    log_steps = 1000
-    last_reward = None
+    total_episode_rewards = []
+    episode_rewards = []    
+
+    log_steps = 10000
+    log_file = "%s/%s" % (args.output_dir, args.log_file)
+    with open(log_file) as lfp:
+        print("Timestep", "Mean Reward", "Best Reward", file=lfp)
  
+
     saver = tf.train.Saver()
     if args.task == 'DuskDrive':
         last_obs = env.reset()
     else:
         last_obs = env.reset(relaunch=True)
-    log_file = "%s/%s" % (args.output_dir, args.log_file)
 
-    lfp = open(log_file, 'w')
-    lfp.write("Timestep\tMean Reward\tBest Mean Reward\tEpisodes\tExploration\tLearning Rate\n")
-    lfp.close()
-
-    if args.weights:
+    if args.weights
         model_initialized = True
-        print('loaded weights ' + args.weights)
         saver.restore(session, args.weights)
 
-    if args.render:
-        env.render()
 
     for t in itertools.count():
+        if args.render:
+            env.render()
 
         if t % args.save_period == 0 and model_initialized:
             save_path = saver.save(session, os.path.join(args.snapshot_dir, "model_%s.ckpt" %(str(t))))
@@ -210,43 +205,31 @@ def train(env, session, args,
                 reward = reward_from_obs(last_obs, reward, done)
             continue
 
+      
         if args.task == "DuskDrive":
             down_samp = imresize(last_obs_image, (128, 128, 3))
         elif args.task == "Torcs":
             down_samp = last_obs_image.reshape(64, 64, 3)
         elif args.task == "Torcs_novision":
             down_samp = last_obs_image.reshape(1, 1, -1)
-        obs_idx = replay_buffer.store_frame(down_samp)
 
         # Loading ReplayBuffer with actions that are:
         #   1. Random with probability exploration_schedule.value(t)
         #   2. Chosen from the target DQN
+	
+        obs_idx = replay_buffer.store_frame(down_samp)
+        eps = args.exploration_schedule.value(t)
 
-        if (random.random() < args.exploration_schedule.value(t) or not model_initialized):
-            action_idx = random.randint(0,num_actions-1)
-            #print('random', action_idx)
+        if np.random.rand(1) < eps or (not model_initialized):
+            action_idx = np.random.choice(num_actions)
         else:
-            encoded_obs = replay_buffer.encode_recent_observation()
-            encoded_obs = encoded_obs[np.newaxis, ...]
+            encoded_obs = replay_buffer.encode_recent_observation()[np.newaxis,...]
             q_net_eval = session.run(q_net, feed_dict={obs_t_ph: encoded_obs})
             action_idx = np.argmax(q_net_eval)
-            #print(action_idx, q_net_eval)
 
         last_obs, reward, done, info = env.step([actions[action_idx]])
-        #reward = reward_from_obs(last_obs, reward, done)
-        #reward = -reward
-        #print('reward: ' + str(reward))
         
-        if not args.velocity:
-            last_reward = reward[0] if args.task == "DuskDrive" else reward
-        else:
-            if not last_reward or reward[0] >= last_reward:
-	        last_reward = reward[0]
-            else:
-                # adding penalty for negative changes in reward
-		last_reward = reward[0] + (reward[0] - last_reward)
-
-
+        last_reward = reward[0] if args.task == "DuskDrive" else reward
         episode_rewards.append(last_reward)
         replay_buffer.store_effect(obs_idx, action_idx, last_reward, done)
 
@@ -256,14 +239,17 @@ def train(env, session, args,
                 last_obs = env.reset()
             else:
                 last_obs = env.reset(relaunch=True)
+	    total_episode_rewards.append(episode_rewards)
             episode_rewards = []
-
 
         # Performing Experience Replay by sampling from the ReplayBuffer
         # and training the network with some random exploration criterion
 
-        if t > learning_starts and t % learning_freq == 0 and replay_buffer.can_sample(batch_size):
-            obs_batch, act_batch, rew_batch, next_obs_batch, done_mask = replay_buffer.sample(batch_size)
+        if t > learning_starts and 
+	   t % learning_freq == 0 and 
+	   replay_buffer.can_sample(batch_size):
+            
+	    obs_batch, act_batch, rew_batch, next_obs_batch, done_mask = replay_buffer.sample(batch_size)
             
             if not model_initialized:
                 model_initialized = True
@@ -278,31 +264,22 @@ def train(env, session, args,
                         done_mask_ph: done_mask,
                         lr: args.optimizer.lr_schedule.value(t)
                         }
-            summary, _ = session.run([merged, train_fn], feed_dict=train_dict)
-            train_writer.add_summary(summary, t)
+            num_param_updates += 1
 
-            if t % target_update_freq == 0:
-                print('updating target networks...')
+            if num_param_updates % target_update_freq == 0:
                 session.run(update_target_fn)
-                num_param_updates += 1            
 
 
             # Logging
             if len(episode_rewards) > 0:
-                mean_episode_reward = np.mean(episode_rewards)
+                t_rew = np.array(total_episode_rewards)
+                mean_episode_reward = np.mean(t_rew[-100:])
             if len(episode_rewards) > 100:
                 best_mean_episode_reward = max(best_mean_episode_reward, mean_episode_reward)
-
             if t % log_steps == 0 and model_initialized:
-                if best_mean_episode_reward != -float('inf'):
-                    lfp = open(log_file, 'a')
-                    lfp.write("%s\t%s\t%s\t%s\t%s\t%s\n" %(t, 
-                        mean_episode_reward, 
-                        best_mean_episode_reward, 
-                        len(episode_rewards),
-                        args.exploration_schedule.value(t),
-                        args.optimizer.lr_schedule.value(t)))
-                    lfp.close()
+                with open(log_file,'w') as lfp:
+		     print(t, mean_episode_reward, best_mean_episode_reward, file=lfp)
+			
     
 def get_session(gpu_id):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
@@ -342,12 +319,12 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=4, help='gpu id')
     parser.add_argument('--model', type=str, default="BaseDQN", help="type of network model for the Q network")
-    parser.add_argument('--output_dir', type=str, default="output_full_test2/", help="where to store all misc. training output")
+    parser.add_argument('--output_dir', type=str, default="output_full/", help="where to store all misc. training output")
     parser.add_argument('--task', type=str, choices=['DuskDrive', 'Torcs', 'Torcs_novision'], default="DuskDrive")
     parser.add_argument('--lr_mult', type=float, default=1.0, help='learning rate multiplier')
     parser.add_argument('--weights', type=str, default="output_full/weights/model_7000000.ckpt", help="path to model weights")
     parser.add_argument('--max_iters', type=int, default=20000, help='number of timesteps to run DQN')
-    parser.add_argument('--log_file', type=str, default="train.log", help="where to log DQN output")
+    parser.add_argument('--log_file', type=str, default="output_full/train.log", help="where to log DQN output")
     parser.add_argument('--render', action='store_true', help='If true, will call env.render()')
     parser.add_argument('--velocity', action='store_true', help='velocity constraint for dusk drive')
     parser.add_argument('--save_period', type=int, default=1e6, help='period of saving checkpoints')
